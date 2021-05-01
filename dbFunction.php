b5ff1a611e32b8f50a8d60bc1a0e5e9f59648dec
Jesus Melo <jvmelo474@gmail.com>
2021-05-01 11:38:56 -0400

    Finishing functionalities.

--- dbFunction.php
+++ dbFunction.php
@@ -1,6 +1,7 @@
 <?php
 
-function dbConnect(){
+function newDBConnect()
+{
     $servername = "localhost";
     $username = "pruebaphp";
     $password = "pruebaphp";
@@ -15,4 +16,54 @@ function dbConnect(){
     return $conn;
 }
 
-?>
+function selectAll($table)
+{
+
+    $conn = newDBConnect();
+    $sql = "SELECT * FROM $table";
+    $results = $conn->query($sql);
+    $records = [];
+
+    if ($results && $results->num_rows > 0) {
+
+        while ($record = $results->fetch_assoc()) {
+            $records[] = $record;
+        }
+    }
+
+    $conn->close();
+    return $records;
+}
+
+function insertRecord($sql)
+{
+    $conn = newDBConnect();
+    $result = true;
+
+    if (mysqli_query($conn, $sql) === FALSE) {
+        $result = false;
+    }
+
+    $conn->close();
+    return $result;
+}
+
+function queryInsert()
+{
+    insertRecord("DROP TABLE IF EXISTS main_anime");
+    insertRecord("CREATE TABLE main_anime (id INT AUTO_INCREMENT NOT NULL, element_name text, parent_id INT, PRIMARY KEY (id))");
+    insertRecord("INSERT INTO main_anime (element_name, parent_id) VALUES ('Naruto', '0')");
+    insertRecord("INSERT INTO main_anime (element_name, parent_id) VALUES ('Naruto Shippuden', '1')");
+    insertRecord("INSERT INTO main_anime (element_name, parent_id) VALUES ('Boruto', '2')");
+    insertRecord("INSERT INTO main_anime (element_name, parent_id) VALUES ('Boruto Movie', '2')");
+    insertRecord("INSERT INTO main_anime (element_name, parent_id) VALUES ('Naruto Movie 1', '1')");
+    insertRecord("INSERT INTO main_anime (element_name, parent_id) VALUES ('Naruto Movie 2', '5')");
+    insertRecord("INSERT INTO main_anime (element_name, parent_id) VALUES ('Naruto Movie 3', '6')");
+    insertRecord("INSERT INTO main_anime (element_name, parent_id) VALUES ('Naruto Movie 4', '7')");
+
+    insertRecord("INSERT INTO main_anime (element_name, parent_id) VALUES ('Attack on Titan', '0')");
+    insertRecord("INSERT INTO main_anime (element_name, parent_id) VALUES ('Attack on Titan S2', '9')");
+    insertRecord("INSERT INTO main_anime (element_name, parent_id) VALUES ('Attack on Titan S3', '10')");
+    insertRecord("INSERT INTO main_anime (element_name, parent_id) VALUES ('Attack on Titan S4 Part 1', '11')");
+    insertRecord("INSERT INTO main_anime (element_name, parent_id) VALUES ('Attack on Titan S4 Part 1', '11')");
+}
--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
+ include "dbFunction.php";
+ queryInsert();
+ 
+ $records = selectAll("main_anime");
+ 
+
+function draw($dataset, $roots, $depth = 0){
+    foreach($roots as $root){
+        echo str_repeat("&nbsp;&nbsp;&nbsp;&nbsp;", $depth) . " - " . $root['element_name'] . "<br>";
+        $newroots = array_filter($dataset, function($d) use ($root){
+            return $d['parent_id'] == $root['id'];
+        });
+        if(count($newroots)){
+            draw($dataset, $newroots, $depth + 1);
+        }
+    }
+
+}
+
+$roots = array_filter($records, function($d){
+    return $d['parent_id'] == 0;
+});
+
+draw($records, $roots);
 
 ?>
